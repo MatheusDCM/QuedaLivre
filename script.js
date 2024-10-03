58ace31a84f7bc65cbc162974cc35efae24fc598
MatheusDCM <matheus.moretti@escola.pr.gov.br>
2024-10-03 09:21:00 -0300

    commit

--- script.js
+++ script.js
@@ -1,26 +1,6 @@
 let intervalo;
 let emQueda = false;
 let pausado = false;
-let arrastando = false;
-
-const objeto = document.getElementById('objeto');
-
-objeto.addEventListener('mousedown', (e) => {
-    arrastando = true;
-    objeto.style.cursor = 'grabbing';
-});
-
-document.addEventListener('mousemove', (e) => {
-    if (arrastando && !emQueda) {
-        objeto.style.left = `${e.clientX - objeto.offsetWidth / 2}px`;
-        objeto.style.top = `${e.clientY - objeto.offsetHeight / 2}px`;
-    }
-});
-
-document.addEventListener('mouseup', () => {
-    arrastando = false;
-    objeto.style.cursor = 'grab';
-});
 
 function Queda() {
     if (emQueda) {
@@ -31,24 +11,25 @@ function Queda() {
 }
 
 function iniciarQueda() {
-    let altura = 0;
+    const objeto = document.getElementById('objeto');
+    let altura = 0; 
     const g = 9.8;
     let tempo = 0;
-    const maxAltura = 300;
-
+    const maxAltura = 300; 
+    
     intervalo = setInterval(() => {
         if (!pausado) {
             tempo += 0.05;
-            altura = 0.5 * g * Math.pow(tempo, 2);
+            altura = 0.5 * g * Math.pow(tempo, 2); 
 
             if (altura > maxAltura) {
-                altura = maxAltura;
+                altura = maxAltura; 
             }
 
             const velocidade = g * tempo;
-            objeto.style.top = (100 + altura) + 'px';
+            objeto.style.top = (100 + altura) + 'px'; 
             document.getElementById('info').innerText = `Altura: ${altura.toFixed(2)} m | Tempo: ${tempo.toFixed(2)} s | Velocidade: ${velocidade.toFixed(2)} m/s`;
-
+            
             if (altura >= maxAltura) {
                 clearInterval(intervalo);
                 emQueda = false;
@@ -67,7 +48,8 @@ function pausar() {
 
 function reiniciar() {
     clearInterval(intervalo);
-    objeto.style.top = '100px';
+    const objeto = document.getElementById('objeto');
+    objeto.style.top = '100px'; 
     document.getElementById('info').innerText = '';
     emQueda = false;
     pausado = false;

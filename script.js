8395c8716ccf3e9d6d5e581436d951db6172a082
MatheusDCM <matheus.moretti@escola.pr.gov.br>
2024-10-03 09:14:21 -0300

    commit

--- script.js
+++ script.js
@@ -1,104 +1,58 @@
-<!DOCTYPE html>
-<html lang="pt-BR">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Queda Livre</title>
-    <style>
-        #objeto {
-            position: absolute;
-            top: 100px;
-            left: 100px;
-            width: 50px;
-            height: 50px;
-            background-color: red;
-            cursor: grab;
-        }
-    </style>
-</head>
-<body>
-    <div id="objeto"></div>
-    <button id="botao" onclick="Queda()">Iniciar Queda</button>
-    <button id="botaoPausa" onclick="pausar()">Pausar</button>
-    <div id="info"></div>
+let intervalo;
+let emQueda = false;
+let pausado = false;
 
-    <script>
-        let intervalo;
-        let emQueda = false;
-        let pausado = false;
-        let arrastando = false;
+function Queda() {
+    if (emQueda) {
+        reiniciar();
+    } else {
+        iniciarQueda();
+    }
+}
 
-        const objeto = document.getElementById('objeto');
+function iniciarQueda() {
+    const objeto = document.getElementById('objeto');
+    let altura = 0; 
+    const g = 9.8;
+    let tempo = 0;
+    const maxAltura = 300; 
+    
+    intervalo = setInterval(() => {
+        if (!pausado) {
+            tempo += 0.05;
+            altura = 0.5 * g * Math.pow(tempo, 2); 
 
-        objeto.addEventListener('mousedown', (e) => {
-            arrastando = true;
-            objeto.style.cursor = 'grabbing';
-        });
-
-        document.addEventListener('mousemove', (e) => {
-            if (arrastando && !emQueda) {
-                objeto.style.left = `${e.clientX - objeto.offsetWidth / 2}px`;
-                objeto.style.top = `${e.clientY - objeto.offsetHeight / 2}px`;
+            if (altura > maxAltura) {
+                altura = maxAltura; 
             }
-        });
 
-        document.addEventListener('mouseup', () => {
-            arrastando = false;
-            objeto.style.cursor = 'grab';
-        });
-
-        function Queda() {
-            if (emQueda) {
-                reiniciar();
-            } else {
-                iniciarQueda();
+            const velocidade = g * tempo;
+            objeto.style.top = (100 + altura) + 'px'; 
+            document.getElementById('info').innerText = `Altura: ${altura.toFixed(2)} m | Tempo: ${tempo.toFixed(2)} s | Velocidade: ${velocidade.toFixed(2)} m/s`;
+            
+            if (altura >= maxAltura) {
+                clearInterval(intervalo);
+                emQueda = false;
+                document.getElementById('botao').innerText = 'Reiniciar';
             }
         }
+    }, 50);
+    emQueda = true;
+    document.getElementById('botao').innerText = 'Parar Queda';
+}
 
-        function iniciarQueda() {
-            let altura = 0;
-            const g = 9.8;
-            let tempo = 0;
-            const maxAltura = 300;
+function pausar() {
+    pausado = !pausado;
+    document.getElementById('botaoPausa').innerText = pausado ? 'Retomar' : 'Pausar';
+}
 
-            intervalo = setInterval(() => {
-                if (!pausado) {
-                    tempo += 0.05;
-                    altura = 0.5 * g * Math.pow(tempo, 2);
-
-                    if (altura > maxAltura) {
-                        altura = maxAltura;
-                    }
-
-                    const velocidade = g * tempo;
-                    objeto.style.top = (100 + altura) + 'px';
-                    document.getElementById('info').innerText = `Altura: ${altura.toFixed(2)} m | Tempo: ${tempo.toFixed(2)} s | Velocidade: ${velocidade.toFixed(2)} m/s`;
-
-                    if (altura >= maxAltura) {
-                        clearInterval(intervalo);
-                        emQueda = false;
-                        document.getElementById('botao').innerText = 'Reiniciar';
-                    }
-                }
-            }, 50);
-            emQueda = true;
-            document.getElementById('botao').innerText = 'Parar Queda';
-        }
-
-        function pausar() {
-            pausado = !pausado;
-            document.getElementById('botaoPausa').innerText = pausado ? 'Retomar' : 'Pausar';
-        }
-
-        function reiniciar() {
-            clearInterval(intervalo);
-            objeto.style.top = '100px';
-            document.getElementById('info').innerText = '';
-            emQueda = false;
-            pausado = false;
-            document.getElementById('botao').innerText = 'Iniciar Queda';
-            document.getElementById('botaoPausa').innerText = 'Pausar';
-        }
-    </script>
-</body>
-</html>
+function reiniciar() {
+    clearInterval(intervalo);
+    const objeto = document.getElementById('objeto');
+    objeto.style.top = '100px'; 
+    document.getElementById('info').innerText = '';
+    emQueda = false;
+    pausado = false;
+    document.getElementById('botao').innerText = 'Iniciar Queda';
+    document.getElementById('botaoPausa').innerText = 'Pausar';
+}
